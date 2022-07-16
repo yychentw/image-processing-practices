56529e3ec70b897e39d5872ea7756e5980b90264
yychentw <73382240+yychentw@users.noreply.github.com>
2022-07-16 23:23:17 +0800

    Add files via upload
    Upload two MATLAB scripts and two image files which must be used in the programs.
    Each program resolved a problem set by Hao-Chun Hsu.
    `excercise_1_identify_leaf_contour.m` identified the contour of the maple leaf without the petiole in image `Maple.jpg`.
    `excercise_2_label_coins.m` labeled the coins of different denominations with different colors, and calculated their mean areas.

---
+++ exercise_1_identify_leaf_contour.m
@@ -0,0 +1,43 @@
+﻿%{
+Exercise 1 set by Hao-Chun Hsu
+Write a MATLAB script to identify the contour of the maple leaf in image ‘Maple.jpg’. The program should be able to exclude the petiole from the contour. Draw the contour on top of the maple leaf in the image. Please also calculate the ratio of leaf contour and leaf area.
+%}
+
+clear all
+close all
+
+% Convert image to binary image based on threshold
+I = imread('Maple.jpg');
+grayI = rgb2gray(I);
+level = graythresh(I);
+thresh = round(level*255)
+BW = im2bw(grayI, level);
+figure;
+subplot(1, 3, 1), imshow(grayI);
+subplot(1, 3, 2); imhist(grayI);
+line([thresh thresh], [0 300]);
+subplot(1, 3, 3); imshow(BW);
+
+# Complement the binary image and exclude the petiole by image opening
+newBW = imcomplement(BW);
+openedBW = imopen(newBW, strel('octagon', 6));
+figure; 
+subplot(1, 2, 1); imshow(newBW);
+subplot(1, 2, 2); imshow(openedBW);
+
+% Identify the contour and draw it on top of the leaf in the image
+Icontour = I;
+contour = bwperim(openedBW, 8);
+num_pixels = prod(size(BW));
+c_index = find(contour == 1);
+Icontour([c_index c_index+num_pixels c_index+num_pixels*2]) = 0;
+figure; 
+subplot(1, 2, 1); imshow(contour);
+subplot(1, 2, 2); imshow(Icontour);
+imwrite(contour, 'Maple_bw_contour.jpg')
+imwrite(Icontour, 'Maple_with_contour.jpg')
+
+% Calculate the ratio of leaf contour and area
+area = sum(openedBW(:))
+contour_length = sum(contour(:))
+ratio = contour_length/area
---
+++ exercise_2_label_coins.m
@@ -0,0 +1,78 @@
+﻿%{
+Exercise 2 set by Hao-Chun Hsu
+Calculate the mean area of 1 NTD, 5 NTD, 10 NTD and 50 NTD coins in image ‘NTDcoins.jpg’. Demonstrate the coins in a labeled image, where black pixels represent background, red pixels represent 1 NTD coins, yellow pixels represent 5 NTD coins, green pixels represent 10 NTD coins, and blue pixels represent 50 NTD coins. Describe your image processing procedure and approach.
+%}
+
+clear all
+close all
+
+pkg load statistics
+
+% Convert image to binary image based on threshold
+I = imread('NTDcoins.jpg');
+grayI = rgb2gray(I);
+level = graythresh(grayI);
+thresh = round(level*255)
+BW = im2bw(grayI, level);
+figure; 
+subplot(1, 2, 1); imhist(grayI); line([thresh thresh], [0 10000]);
+subplot(1, 2, 2); imshow(BW);
+
+% Noise reduction by removing extremely small connected objects
+[labeled, numObjects] = bwlabel(BW, 8);
+objdata = regionprops(labeled, 'basic');
+[maxvalue, argmax] = max([objdata.Area]);
+BW(find((labeled ~= argmax) & (labeled ~= 0))) = 0;
+figure; imshow(BW);
+
+comBW = imcomplement(BW);
+[labeled, numObjects] = bwlabel(comBW, 8);
+objdata = regionprops(labeled, 'basic');
+[idx, C] = kmeans([objdata.Area]', 2);
+[minvalue, argmin] = min(C);
+comBW(ismember(labeled, find(idx == argmin))) = 0;
+figure; imshow(comBW);
+
+% Noise reduction using image closing (not work well...)
+%{
+comBW = imcomplement(BW);
+closedBW = imclose(comBW, strel('diamond', 2));
+figure; 
+subplot(1, 2, 1); imshow(comBW);
+subplot(1, 2, 2); imshow(closedBW);
+%}
+
+% Label connected components and obtain coins' area
+[labeled, numObjects] = bwlabel(comBW, 8);
+coindata = regionprops(labeled, 'basic');
+areas = [coindata.Area]';
+figure; hist(areas);
+
+% Separate different coins by KMeans (reset index according to sorted area)
+[idx, C] = kmeans(areas, 4);
+[sorted_mean_areas, sorted_idx] = sort(C);
+new_idx = zeros(size(idx));
+for ii = 1:4
+    new_idx(find(idx == sorted_idx(ii))) = ii;
+end
+
+% Demonstrate the coins in a labeled image
+newI = uint8(zeros(size(I)));
+num_pixels = prod(size(BW));
+for ii = 1:4
+    selected_idx = find(ismember(labeled, find(new_idx == ii)));
+    if ii == 1
+        newI(selected_idx) = 255;
+    elseif ii == 2
+        newI([selected_idx selected_idx+num_pixels]) = 255;
+    elseif ii == 3
+        newI(selected_idx+num_pixels) = 255;
+    else
+        newI(selected_idx+num_pixels*2) = 255;
+    end
+end
+figure; imshow(newI);
+imwrite(newI, 'labeled_coins.jpg');
+
+% Mean area of 1 NTD, 5 NTD, 10 NTD, and 50 NTD coins
+sorted_mean_areas
